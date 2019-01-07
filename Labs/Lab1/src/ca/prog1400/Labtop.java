0fd9aedbf3a1552951e4eb14c19ff5baa79d4a74
Qikongweb <kongqi2011@gmail.com>
2019-01-07 10:17:13 -0400

    Added System out

---
+++ Labs/Lab1/src/ca/prog1400/Labtop.java
@@ -0,0 +1,9 @@
+package ca.prog1400;
+
+public class Labtop {
+    double price = 1000000.99;
+    String brand = "IBM";
+    boolean isDisk = true;
+
+
+}
--- Labs/Lab1/src/ca/prog1400/Main.java
+++ Labs/Lab1/src/ca/prog1400/Main.java
@@ -3,6 +3,14 @@ package ca.prog1400;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Prog 1400!!");
+        String courseTitle = "Prog 1400";
+        boolean isRaining = false;
+        int temp = 20;
+        int foo;
+        float hst = 0.15f;
+        double gst = 0.09;
+        char x = 'x';
+
+	    System.out.println(courseTitle);
     }
 }
