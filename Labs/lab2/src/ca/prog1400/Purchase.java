96b3345c74902bd93d3dbf6ad7cabf46eae00ce9
Qikongweb <kongqi2011@gmail.com>
2019-01-10 12:13:12 -0400

    lab2

---
+++ Labs/lab2/src/ca/prog1400/Purchase.java
@@ -0,0 +1,24 @@
+package ca.prog1400;
+import java.util.Scanner;
+
+
+public class Purchase {
+
+    public void calculateShipping() {
+
+        System.out.println("Welcome to our shipping charging calculator!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your total purchase: ");
+        double totalPur = scanner.nextInt();
+        if (totalPur < 50){
+            totalPur +=10;
+            System.out.println("Your shipping fee is: $10.0");
+        }
+        else {
+            System.out.println("Your final total costs is: free");
+        }
+        System.out.println("Your final total costs is: $" + totalPur);
+
+    }
+
+}
